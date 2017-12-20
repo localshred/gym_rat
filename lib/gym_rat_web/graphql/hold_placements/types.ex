3a34ac888ca87101b81c78e836168fe9ce8de5eb
BJ Neilsen <bj.neilsen@gmail.com>
2017-12-19 20:07:59 -0700

    Wire up HoldPlacement.grid_coordinate resolver

--- lib/gym_rat_web/graphql/hold_placements/types.ex
+++ lib/gym_rat_web/graphql/hold_placements/types.ex
@@ -1,19 +1,31 @@
 defmodule GymRatWeb.Graphql.HoldPlacements.Types do
   use Absinthe.Schema.Notation
 
+  object :grid_coordinate do
+    field :x, non_null(:string) do
+      resolve fn (hold_placement, _args, _context) ->
+        hold_placement.grid_coordinate_x
+      end
+    end
+    field :y, non_null(:string) do
+      resolve fn (hold_placement, _args, _context) ->
+        hold_placement.grid_coordinate_x
+      end
+    end
+  end
+
   object :hold_placement do
     field :id, non_null(:id)
     field :route, non_null(:route)
     field :hold, non_null(:hold)
-    field :grid_coordinate, :grid_coordinate
+    field :grid_coordinate, :grid_coordinate do
+      resolve fn (hold_placement, _args, _context) ->
+        hold_placement
+      end
+    end
     field :is_start, non_null(:boolean)
     field :is_finish, non_null(:boolean)
     field :inserted_at, non_null(:utc_timestamp)
     field :updated_at, non_null(:utc_timestamp)
-  end
-
-  object :grid_coordinate do
-    field :x, non_null(:string)
-    field :y, non_null(:string)
   end
 end
