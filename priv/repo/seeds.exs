a57345b97393b54ada04f3f883e4e1891203112a
BJ Neilsen <bj.neilsen@gmail.com>
2017-12-22 14:20:03 -0700

    Add seeds for grades

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,37 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     GymRat.Repo.insert!(%GymRat.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule SeedsHelper do
+  @now DateTime.utc_now()
+
+  def build_grade(system, major, minor \\ nil, difficulty \\ nil) do
+    %{
+      system: system,
+      major: major,
+      minor: minor,
+      difficulty: difficulty,
+      inserted_at: @now,
+      updated_at: @now
+    }
+  end
+end
+
+yds_minor_grades = ["a", "a/b", "b", "b/c", "c", "c/d", "d"]
+
+hueco_grades = Enum.map(0..17, &(SeedsHelper.build_grade("hueco", Integer.to_string(&1))))
+yds_base_grades = Enum.map(0..9, &(SeedsHelper.build_grade("yds", Integer.to_string(&1))))
+yds_advanced_grades = Enum.flat_map(
+  10..15,
+  fn major -> Enum.map(
+    yds_minor_grades,
+    fn minor -> SeedsHelper.build_grade("yds", Integer.to_string(major), minor) end
+  )
+  end
+)
+
+grades = Enum.concat([
+  hueco_grades,
+  yds_base_grades,
+  yds_advanced_grades
+])
+
+grades_result = GymRat.Repo.insert_all(GymRat.RouteManagement.Grade, grades)
+
+IO.inspect(grades_result)
