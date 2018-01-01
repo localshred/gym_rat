7160dc3fc7d85638baebc59816d841e2b63189f2
BJ Neilsen <bj.neilsen@gmail.com>
2017-12-31 23:30:55 -0700

    Add tests for Grade queries and mutations

---
+++ test/gym_rat_web/graphql/grades/mutations_test.exs
@@ -0,0 +1,243 @@
+defmodule GymRatWeb.Graphql.Grades.MutationsTest do
+  use GymRatWeb.ConnCase
+
+  import GymRat.TestFactories
+
+  alias GymRat.RouteManagement
+  alias GymRat.Lore
+
+  describe "create_grade" do
+    test "creates a grade with the given parameters" do
+      query_name = "createGrade"
+
+      query = """
+        mutation #{query_name}(
+          $system: GradeSystem!,
+          $major: String!,
+          $minor: String!,
+          $difficulty: GradeDifficulty!
+        ) {
+          createGrade(grade: {
+            system: $system,
+            major: $major,
+            minor: $minor,
+            difficulty: $difficulty
+          }) {
+            grade {
+              id
+              system
+              major
+              minor
+              difficulty
+            }
+          }
+        }
+      """
+
+      run_options = [
+        query: query,
+        query_name: query_name,
+        variables: %{
+          "system" => "YDS",
+          "major" => "11",
+          "minor" => "c",
+          "difficulty" => "HARD"
+        }
+      ]
+
+      before_count = RouteManagement.count_grades()
+
+      run_options
+      |> graphql_run()
+      |> Lore.path([:data, "createGrade", "grade"])
+      |> assert_grade(run_options[:variables])
+
+      after_count = RouteManagement.count_grades()
+      assert before_count + 1 == after_count
+    end
+  end
+
+  describe "delete_grade" do
+    test "deletes a grade by ID" do
+      grade = insert(:grade)
+      query_name = "deleteGrade"
+
+      query = """
+        mutation #{query_name}(
+          $id: ID!
+        ) {
+          deleteGrade(query: { id: $id }) {
+            success
+            deletedCount
+          }
+        }
+      """
+
+      run_options = [
+        query: query,
+        query_name: query_name,
+        variables: %{"id" => to_string(grade.id)}
+      ]
+
+      before_count = RouteManagement.count_grades()
+
+      delete_result =
+        run_options
+        |> graphql_run()
+        |> Lore.path([:data, "deleteGrade"])
+
+      assert delete_result["success"] == true
+      assert delete_result["deletedCount"] == 1
+
+      after_count = RouteManagement.count_grades()
+      assert before_count - 1 == after_count
+    end
+
+    test "returns false success and 0 deletedCount when given grade ID doesn't exist" do
+      grade_id = -1
+
+      assert RouteManagement.count_grades() == 0
+
+      query_name = "deleteGrade"
+
+      query = """
+        mutation #{query_name}(
+          $id: ID!
+        ) {
+          deleteGrade(query: { id: $id }) {
+            success
+            deletedCount
+          }
+        }
+      """
+
+      run_options = [
+        query: query,
+        query_name: query_name,
+        variables: %{"id" => to_string(grade_id)}
+      ]
+
+      delete_result =
+        run_options
+        |> graphql_run()
+        |> Lore.path([:data, "deleteGrade"])
+
+      assert delete_result["success"] == false
+      assert delete_result["deletedCount"] == 0
+      assert RouteManagement.count_grades() == 0
+    end
+  end
+
+  describe "update_grade" do
+    test "updates an existing grade with the given parameters" do
+      existing_grade = insert(:grade)
+      query_name = "updateGrade"
+
+      query = """
+        mutation #{query_name}(
+          $id: ID!,
+          $system: GradeSystem!,
+          $major: String!,
+          $minor: String!,
+          $difficulty: GradeDifficulty!
+        ) {
+          updateGrade(query: { id: $id }, grade: {
+            system: $system,
+            major: $major,
+            minor: $minor,
+            difficulty: $difficulty
+          }) {
+            grade {
+              id
+              system
+              major
+              minor
+              difficulty
+            }
+          }
+        }
+      """
+
+      run_options = [
+        query: query,
+        query_name: query_name,
+        variables: %{
+          "id" => to_string(existing_grade.id),
+          "system" => "YDS",
+          "major" => "14",
+          "minor" => "a",
+          "difficulty" => "BENCHMARK"
+        }
+      ]
+
+      before_count = RouteManagement.count_grades()
+
+      run_options
+      |> graphql_run()
+      |> Lore.path([:data, "updateGrade", "grade"])
+      |> assert_grade(run_options[:variables])
+
+      after_count = RouteManagement.count_grades()
+      assert before_count == after_count
+    end
+
+    test "returns null when the grade doesn't exist with the given ID" do
+      grade_id = -1
+
+      assert RouteManagement.count_grades() == 0
+
+      query_name = "updateGrade"
+
+      query = """
+        mutation #{query_name}(
+          $id: ID!,
+          $system: GradeSystem!,
+          $major: String!,
+          $minor: String!,
+          $difficulty: GradeDifficulty!
+        ) {
+          updateGrade(query: { id: $id }, grade: {
+            system: $system,
+            major: $major,
+            minor: $minor,
+            difficulty: $difficulty
+          }) {
+            grade {
+              id
+              system
+              major
+              minor
+              difficulty
+            }
+          }
+        }
+      """
+
+      run_options = [
+        query: query,
+        query_name: query_name,
+        variables: %{
+          "id" => to_string(grade_id),
+          "system" => "YDS",
+          "major" => "15",
+          "minor" => "c",
+          "difficulty" => "HARD"
+        }
+      ]
+
+      run_options
+      |> graphql_run(:expect_errors)
+      |> assert_contains_error("Unable to update grade")
+
+      assert RouteManagement.count_grades() == 0
+    end
+  end
+
+  def assert_grade(actual, expected) do
+    assert actual["id"] != nil
+    assert actual["system"] == String.upcase(expected["system"])
+    assert actual["major"] == expected["major"]
+    assert actual["minor"] == expected["minor"]
+    assert actual["difficulty"] == String.upcase(expected["difficulty"])
+  end
+end
---
+++ test/gym_rat_web/graphql/grades/queries_test.exs
@@ -0,0 +1,245 @@
+defmodule GymRatWeb.Graphql.Grades.QueriesTest do
+  use GymRatWeb.ConnCase
+
+  import GymRat.TestFactories
+
+  alias GymRat.RouteManagement
+  alias GymRat.Lore
+
+  describe "grade" do
+    test "gets a grade by ID" do
+      expected_grade = insert(:grade)
+
+      query_name = "getGrade"
+
+      query = """
+        query #{query_name}($id: ID!) {
+          grade(query: { id: $id }) {
+            grade {
+              id
+              system
+              major
+              minor
+              difficulty
+              insertedAt
+              updatedAt
+            }
+          }
+        }
+      """
+
+      [
+        query: query,
+        query_name: query_name,
+        variables: %{
+          "id" => to_string(expected_grade.id)
+        }
+      ]
+      |> graphql_run()
+      |> Lore.path([:data, "grade", "grade"])
+      |> assert_grade(expected_grade)
+    end
+
+    test "gets a null grade when given ID that doesn't exist in DB" do
+      grade_id = -1
+
+      assert RouteManagement.count_grades() == 0
+
+      query_name = "getGrade"
+
+      query = """
+        query #{query_name}($id: ID!) {
+          grade(query: { id: $id }) {
+            grade {
+              id
+              system
+              major
+              minor
+              difficulty
+            }
+          }
+        }
+      """
+
+      [
+        query: query,
+        query_name: query_name,
+        variables: %{
+          "id" => to_string(grade_id)
+        }
+      ]
+      |> graphql_run()
+      |> Lore.path([:data, "grade", "grade"])
+      |> (fn grade ->
+            assert grade == nil
+          end).()
+    end
+
+    # test "fetches associated routes" do
+    #   expected_grade = insert(:grade)
+    #   insert_list(3, :route, grade: expected_grade)
+
+    #   query_name = "getgradeRoutes"
+
+    #   query = """
+    #     query #{query_name}($id: ID!) {
+    #       grade(query: { id: $id }) {
+    #         grade {
+    #           id
+    #           routes {
+    #             id
+    #             name
+    #             color
+    #           }
+    #         }
+    #       }
+    #     }
+    #   """
+
+    #   [
+    #     query: query,
+    #     query_name: query_name,
+    #     variables: %{
+    #       "id" => to_string(expected_grade.id)
+    #     }
+    #   ]
+    #   |> graphql_run()
+    #   |> Lore.path([:data, "grade", "grade"])
+    #   |> (fn %{"id" => grade_id, "routes" => routes} ->
+    #         assert grade_id == to_string(expected_grade.id)
+    #         assert length(routes) == 3
+
+    #         Enum.each(routes, fn route ->
+    #           assert route["id"] != nil
+    #           assert route["name"] != nil
+    #         end)
+    #       end).()
+    # end
+  end
+
+  describe "grades" do
+    test "gets all grades when no IDs are given" do
+      expected_grades =
+        insert_list(3, :grade)
+        |> Enum.group_by(fn grade -> grade |> Lore.prop(:id) |> to_string() end)
+
+      query_name = "getGrades"
+
+      query = """
+        query #{query_name} {
+          grades(query: {}) {
+            grades {
+              id
+              system
+              major
+              minor
+              difficulty
+              insertedAt
+              updatedAt
+            }
+          }
+        }
+      """
+
+      [
+        query: query,
+        query_name: query_name
+      ]
+      |> graphql_run()
+      |> Lore.path([:data, "grades", "grades"])
+      |> Enum.each(fn actual_grade ->
+        expected_grade = List.first(expected_grades[actual_grade["id"]])
+        assert_grade(actual_grade, expected_grade)
+      end)
+    end
+
+    test "gets only grades with given IDs" do
+      grades_picked_count = 2
+      grades_total_count = 4
+      grades = insert_list(grades_total_count, :grade)
+
+      grade_ids =
+        grades
+        |> Enum.take(grades_picked_count)
+        |> Enum.map(Lore.prop(:id))
+        |> Enum.map(&to_string/1)
+
+      expected_grades =
+        grades
+        |> Enum.group_by(fn grade -> grade |> Lore.prop(:id) |> to_string() end)
+
+      query_name = "getGrades"
+
+      query = """
+        query #{query_name}(
+          $ids: [String!]!
+        ){
+          grades(query: {
+            ids: $ids
+          }) {
+            grades {
+              id
+              system
+              major
+              minor
+              difficulty
+              insertedAt
+              updatedAt
+            }
+          }
+        }
+      """
+
+      [
+        query: query,
+        query_name: query_name,
+        variables: %{"ids" => grade_ids}
+      ]
+      |> graphql_run()
+      |> Lore.path([:data, "grades", "grades"])
+      |> Lore.each(fn actual_grade ->
+        expected_grade = List.first(expected_grades[actual_grade["id"]])
+        assert_grade(actual_grade, expected_grade)
+      end)
+      |> (fn grades ->
+            assert length(grades) == grades_picked_count
+          end).()
+    end
+
+    test "retrieves empty list when no grades found for given args" do
+      query_name = "getGrades"
+
+      query = """
+        query #{query_name} {
+          grades(query: {}) {
+            grades {
+              id
+            }
+          }
+        }
+      """
+
+      assert RouteManagement.count_grades() == 0
+
+      [
+        query: query,
+        query_name: query_name
+      ]
+      |> graphql_run()
+      |> Lore.path([:data, "grades", "grades"])
+      |> (fn grades ->
+            assert length(grades) == 0
+          end).()
+    end
+  end
+
+  def assert_grade(actual, expected) do
+    assert actual["id"] == to_string(expected.id)
+    assert actual["system"] == String.upcase(expected.system)
+    assert actual["major"] == expected.major
+    assert actual["minor"] == expected.minor
+    assert actual["difficulty"] == String.upcase(expected.difficulty)
+    assert_timestamp(actual["insertedAt"], expected.inserted_at)
+    assert_timestamp(actual["updatedAt"], expected.updated_at)
+  end
+end
